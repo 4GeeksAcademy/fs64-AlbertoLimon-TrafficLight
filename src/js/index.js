c8399ac3ac28013e610ef15bd9192f136b6b3831
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-04-25 05:03:28 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+
 
 //import your own components
-import Home from "./component/home.jsx";
+
+import TrafficLight from "./component/trafficlight.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
